c8c55bd914d6233bcfe2081867dee488a6a142d2
LXiao2015 <m15801021028@163.com>
2018-09-27 15:57:46 +0800

    add bash file for generating C++ from proto

--- server.cc
+++ server.cc
@@ -10,31 +10,31 @@
 
 #include "example.grpc.pb.h"
 
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
+using grpc::Server;    // gRPC server 类
+using grpc::ServerBuilder;    // 构造和启动 gRPC server
+using grpc::ServerContext;    // 为 RPC 传递上下文
+using grpc::Status;    // 记录执行是否成功
 
 // final 关键字表示该类不会被继承
 class SearchRequestImpl final : public SearchService::Service {
-    Status Search(ServerContext* context, const SearchRequest* request, SearchResponse* reply) override {
+    Status Search(ServerContext* context, const SearchRequest* request, SearchResponse* reply) override {    // 服务实现类
         std::string prefix("Hello ");
         reply->set_response(prefix + request->request());
-        return Status::OK;
+        return Status::OK;    // 告知 gRPC 我们已经完成了响应的写入
     }
 };
 
 void RunServer() {
     std::string server_address("0.0.0.0:9001");
-    SearchRequestImpl service;
+    SearchRequestImpl service;    // 创建服务实现类 SearchRequestImpl 的一个实例
 
-    ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
-    std::unique_ptr<Server> server(builder.BuildAndStart());
+    ServerBuilder builder;    // 创建工厂类 ServerBuilder 的一个实例
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());    // 指定监听的地址和端口, 建立不安全的服务器凭据
+    builder.RegisterService(&service);    // 注册服务实例
+    std::unique_ptr<Server> server(builder.BuildAndStart());    // 创建和启动一个 gRPC server
     std::cout << "Server listening on " << server_address << std::endl;
 
-    server->Wait();
+    server->Wait();    // 这个函数只有在服务器关闭或被其他线程调用 Shutdown 关闭时才返回
 }
 
 int main() {
